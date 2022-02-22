5129831cf56a5a45702f82372bfb38a8486253ca
aloggia <aloggia22@gmail.com>
2022-02-21 20:57:09 -0500

    debugging how to strip metadata from the string

--- main.py
+++ main.py
@@ -25,13 +25,16 @@ def send_message(client_socket):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     server_name = input("Enter server name: ")
-    server_port = 1200
+    server_port = 12000
     client_socket = socket(AF_INET, SOCK_STREAM)
     client_socket.connect((server_name, server_port))
 
     # Will need to modify
     client_socket.send("HELLO\n".encode())
     response_message = client_socket.recv(1024)
+    if response_message.startswith("b'") and response_message.endswith("\n'"):
+        response_message = response_message[2:-3]
+    print(response_message)
     if response_message == "HELLO":
         # Put into it's own function
         username = input("Enter student ID/Username: ")
