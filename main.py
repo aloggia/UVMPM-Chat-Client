91ab38dd76ba72a81db3f1bea342ecd4d0316730
aloggia <aloggia22@gmail.com>
2022-02-22 13:03:59 -0500

    Completed implementation of list_users

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from socket import *
 
 # TODO: Function to list online users, function to send a message, function to close connection
 
+
 def list_users(client_socket):
-    users = client_socket.recv(1024)
-    user_list = users.split(',')
+    # Need to craft message to server asking for the user list
+    client_socket.send("List\n".encode())
+    users = client_socket.recv(1024)[7:-1].decode()
+    user_list = users.split(",")
     for user in user_list:
         print(user)
 
@@ -16,6 +19,16 @@ def send_message(client_socket):
     client_socket.send(complete_message.encode())
 
 
+def sign_in(client_socket):
+    username = input("Enter student ID/Username: ")
+    password = input("Enter password: ")
+    client_socket.send(("AUTH:" + username + ":" + password + "\n").encode())
+    while client_socket.recv(1024)[:-1].decode() == "AUTHNO":
+        username = input("Enter student ID/Username: ")
+        password = input("Enter password: ")
+        client_socket.send(("AUTH:" + username + ":" + password + "\n").encode())
+
+
 # List online users - server returns a comma deliminated list - list comprehension to display each name on it's own line
 
 # Send a message - Should be fairly simple
@@ -24,21 +37,21 @@ def send_message(client_socket):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    server_name = input("Enter server name: ")
+    server_name = '132.198.11.12'
     server_port = 12000
     client_socket = socket(AF_INET, SOCK_STREAM)
     client_socket.connect((server_name, server_port))
 
     # Will need to modify
     client_socket.send("HELLO\n".encode())
-    response_message = client_socket.recv(1024)
-    if response_message.startswith("b'") and response_message.endswith("\n'"):
-        response_message = response_message[2:-3]
+    response_message = client_socket.recv(1024)[:-1].decode()
     print(response_message)
     if response_message == "HELLO":
         # Put into it's own function
-        username = input("Enter student ID/Username: ")
-        password = input("Enter password: ")
-        client_socket.send(("AUTH:" + username + ":" + password).encode())
+        # Needs error correcting
+        if client_socket.recv(1024)[:-1].decode() == "AUTHYES":
+            # Put while loop here
+            print("Welcome " + username)
+            list_users(client_socket)
     else:
         print("Error connecting to server")
